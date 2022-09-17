5e99522168186c3e31e839ae5fb7e3fdf30ad13e
gongjianglin6 <gongjlg@yonyou.com>
2022-09-17 10:58:55 +0800

    chore: del

--- exercize-lin/webpack-module/file/sum.js
+++
@@ -1,6 +0,0 @@
-const test = require('./test.js')
-const sum = (a, b) => {
-  console.log("test", test);
-  return a + b
-}
-module.exports = sum
--- exercize-lin/webpack-module/moduleIndex.js
+++
@@ -1,3 +0,0 @@
-const sum = require('./sum.js')
-
-console.log("sum", sum(1, 2));
